68d0521bdd42ab27e64b0902236c8c894c7979f0
M1kss <boytsovs.av@phystech.edu>
2019-01-18 00:10:13 +0300

    Add files via upload

---
+++ Make_tables_no_ctrl.py
@@ -0,0 +1,53 @@
+import sys
+
+
+def write10(chr, pos, NAME, REF, ALT, ER, EA, output10, bed10):
+    ER = str(ER)
+    EA = str(EA)
+    bed10.write(chr + '\t' + str(int(pos) - 1) + '\t' + pos + '\n')
+    output10.write(
+        chr + '\t' + pos + '\t' + NAME + '\t' + REF + '\t' + ALT + '\t' + ER + '\t' + EA + '\t' + '.' + '\t' + '.' + '\t' + '1' + '\t' + '0' + '\n')
+
+
+vcf = open(sys.argv[1], 'r')
+output10 = open(sys.argv[2], 'w')
+
+exp = dict()
+
+Nucleotides = {'A', 'T', 'G', 'C'}
+
+
+def read_from_file(vcf, out):
+    for line in vcf:
+        if line[0] == '#':
+            continue
+        line = line.split()
+        if len(line[3]) == 1 and len(line[4]) == 1:
+            if line[3] in Nucleotides and line[4] in Nucleotides:
+                R = int(line[-1].split(':')[1].split(',')[0])
+                A = int(line[-1].split(':')[1].split(',')[1])
+                NAME = line[2]
+                REF = line[3]
+                ALT = line[4]
+                out[(line[0], line[1])] = (R, A, NAME, REF, ALT)
+
+
+read_from_file(vcf, exp)
+
+skipped = 0
+
+for (chr, pos) in exp.keys():
+    (ER, EA, NAME, REF, ALT) = exp[(chr, pos)]
+
+    if ER == 0:
+        skipped += 1
+        continue
+    else:
+        write10(chr, pos, NAME, REF, ALT, ER, EA, output10, bed10)
+
+for (chr, pos) in ctrl.keys():
+    (CR, CA, NAME, REF, ALT) = ctrl[(chr, pos)]
+    # del ctrl[(chr, pos)]
+    write01(chr, pos, NAME, REF, ALT, CR, CA, output01, bed01)
+
+print('Skipped {} homozigous SNPs'.format(skipped))
