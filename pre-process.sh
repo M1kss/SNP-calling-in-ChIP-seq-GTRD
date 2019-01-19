f3b064637d7219ae9265943ef227d24b9ac20428
M1kss <boytsovs.av@phystech.edu>
2019-01-19 19:53:50 +0300

    Update pre-process.sh

--- pre-process.sh
+++ pre-process.sh
@@ -23,9 +23,9 @@ if [ ! -f $BAMPATH$BAMNAME.bam.bai ]; then
 fi
 	
 echo "Cutting bam.."
-samtools view -b "$BAMPATH/$BAMNAME.bam" chr12 > "$OUT/${BAMNAME}_chop.bam"
-	#chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
-	#chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > "$OUT/${BAMNAME}_chop.bam"
+samtools view -b "$BAMPATH/$BAMNAME.bam" \
+	chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
+	chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > "$OUT/${BAMNAME}_chop.bam"
 
 if [ $? != 0 ]; then
     echo "Failed to cut bam"
